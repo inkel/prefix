dc6d98ebc03d9736851d8a693ac10d54162ee9af
Leandro López <inkel.ar@gmail.com>
2016-10-24 16:11:44 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Don't Repeat Yourself
+
+Prefix a command to avoid repetition.
+
+```
+inkel@miralejos2 ~/dev/go/src/github.com/inkel/prefix $ go build .
+inkel@miralejos2 ~/dev/go/src/github.com/inkel/prefix $ ./prefix git
+git > init
+Initialized empty Git repository in /home/inkel/dev/go/src/github.com/inkel/prefix/.git/
+git > add main.go main_test.go
+git > s
+## Initial commit on master
+A  main.go
+A  main_test.go
+?? prefix
+git > commit -m "initial commit"
+
+You need a passphrase to unlock the secret key for
+user: "Leandro López (inkel) <inkel.ar@gmail.com>"
+4096-bit RSA key, ID 58ABC66B, created 2016-04-08
+
+[master (root-commit) 6727b37] initial commit
+ 2 files changed, 138 insertions(+)
+ create mode 100644 main.go
+ create mode 100644 main_test.go
+git > remote add origin git@github.com:inkel/prefix.git
+git > push -u origin master
+Counting objects: 4, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (4/4), 1.99 KiB | 0 bytes/s, done.
+Total 4 (delta 0), reused 0 (delta 0)
+To git@github.com:inkel/prefix.git
+ * [new branch]      master -> master
+Branch master set up to track remote branch master from origin.
+git > ^D
+```
+
+## TODO
+
+**ERRYTHING**
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"bufio"
+	"github.com/chzyer/readline"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %v command [arguments]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	exe := os.Args[1]
+	args := os.Args[2:]
+	ps := strings.Join(append(os.Args[1:], "> "), " ")
+
+	rl, err := readline.New(ps)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+
+	for {
+		ln, err := rl.Readline()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		parts, _ := Parse(ln)
+
+		cmd := exec.Command(exe, append(args, parts...)...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err = cmd.Run(); err != nil {
+			fmt.Printf("%#v\n", err)
+		}
+	}
+}
+
+func scanner(in string) *bufio.Scanner {
+	var quoted = false
+
+	s := bufio.NewScanner(strings.NewReader(in))
+
+	s.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		start := 0
+
+		// Skip leading spaces.
+		for width := 0; start < len(data); start += width {
+			var r rune
+			r, width = utf8.DecodeRune(data[start:])
+			if !unicode.IsSpace(r) {
+				break
+			}
+		}
+
+		for width, i := 0, start; i < len(data); i += width {
+			var r rune
+			r, width = utf8.DecodeRune(data[i:])
+			if unicode.IsSpace(r) && !quoted {
+				return i + width, data[start:i], nil
+			} else if r == '"' {
+				quoted = !quoted
+				if quoted {
+					return i + width, nil, nil
+				}
+				return i + width, data[start:i], nil
+			}
+		}
+
+		if atEOF && len(data) > start {
+			return len(data), data[start:], nil
+		}
+
+		return start, nil, nil
+	})
+
+	return s
+}
+
+func Parse(in string) ([]string, error) {
+	var parts []string
+
+	s := scanner(in)
+
+	for s.Scan() {
+		parts = append(parts, s.Text())
+	}
+
+	return parts, nil
+}
