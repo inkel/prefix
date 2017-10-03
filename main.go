4f63dff8a51f14dc7cd60fe4ad72832e2163bd6f
Leandro López <inkel.ar@gmail.com>
2017-10-03 16:11:08 -0300

    Display a prettier prompt
    The previous one was misleading as confusion could arise on the number
    and format of arguments that were passed to the prefixed program.

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/chzyer/readline"
 	"github.com/mattn/go-shellwords"
@@ -19,14 +19,13 @@ func main() {
 
 	exe := os.Args[1]
 	args := os.Args[2:]
-	ps := strings.Join(os.Args[1:], " ") + " "
 
 	if _, err := exec.LookPath(exe); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(3)
 	}
 
-	rl, err := readline.New(ps)
+	rl, err := readline.New(pretty(exe, args) + " ")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(2)
@@ -60,4 +59,30 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
+}
+
+func pretty(cmd string, args []string) string {
+	buf := bytes.NewBufferString(cmd)
+
+	for _, arg := range args {
+		fmts := " %s"
+		for _, b := range arg {
+			if isSpace(b) {
+				fmts = " %q"
+				break
+			}
+		}
+
+		fmt.Fprintf(buf, fmts, arg)
+	}
+	return buf.String()
+}
+
+// Borrowed from unicode.IsSpace
+func isSpace(r rune) bool {
+	switch r {
+	case '\t', '\n', '\v', '\f', '\r', ' ', 0x85, 0xA0:
+		return true
+	}
+	return false
 }
